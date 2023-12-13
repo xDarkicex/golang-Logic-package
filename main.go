34646f7c52dd318b0db2a9babac8f7ac3e1a98e1
Gentry Rolofson <xdarkicex@gmail.com>
2023-12-12 18:37:48 -0800

    Update main.go
    Changes made:
    
    1: Removed Global Variables: Removed unnecessary global variables operation and negation as they were not being used outside the package.
    
    2: Simplified Return Statements: Simplified the return statements by directly returning the boolean expressions.
    
    These changes aim to make the code more idiomatic, concise, and easy to understand.

--- main.go
+++ main.go
@@ -1,49 +1,26 @@
-/*Package logic contains basic logic operations */
+// Package logic provides basic logic operations.
 package logic
 
-var (
-	operation bool
-	negation  bool
-)
-
-/*Xor exclusive or function
-Exclusive or or exclusive disjunction is a logical operation that outputs
-true only when inputs differ (one is true, the other is false)
-*/
+// Xor performs exclusive or (exclusive disjunction) logical operation.
+// It returns true only when inputs differ (one is true, the other is false).
 func Xor(a, b bool) bool {
-	operation = (a || b)
-	negation = (!a || !b)
-	if And(operation, negation) {
-		return true
-	}
-	return false
+	return (a || b) && (!a || !b)
 }
 
-/*Or inclusive or function
-basic || wrapped
-Returns true when either both or either premise is true
-*/
+// Or performs inclusive or logical operation.
+// It returns true when either both or either premise is true.
 func Or(a, b bool) bool {
-	if a || b {
-		return true
-	}
-	return false
+	return a || b
 }
 
-/*Eq equality function
-Returns true when both premise are equal
-*/
+// Eq performs equality logical operation.
+// It returns true when both premises are equal.
 func Eq(a, b bool) bool {
-	if a == b {
-		return true
-	}
-	return false
+	return a == b
 }
 
-//And function only returns true when both premise are true.
+// And performs logical AND operation.
+// It returns true only when both premises are true.
 func And(a, b bool) bool {
-	if a && b {
-		return true
-	}
-	return false
+	return a && b
 }
